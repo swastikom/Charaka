a257db81d51401c45d69a8e7691456d4d08e72bc
Swastik Dhar <swastikdhar18@gmail.com>
2023-09-01 20:47:19 +0530

    model ready...

--- training/model_test.py
+++ training/model_test.py
@@ -2,10 +2,11 @@ import pickle
 import pandas as pd
 import numpy as np
 
-
+# Load the model, encoder, and disease_freq from the pickle file
 with open('./training/model/prediction.pkl', 'rb') as file:
-    loaded_model, encoder, disease_freq = pickle.load(file)
+    clf,ferver_encoder,cough_encoder,fatigue_encoder,breathing_encoder,blood_encoder,fat_encoder,gender_encoder,outcome_encoder,category_counts = pickle.load(file)
 
+# Define the new data
 data_for_outcome = {
     'Fever': ['Yes'],
     'Cough': ['No'],
@@ -18,20 +19,30 @@ data_for_outcome = {
     'Disease_freq': ['Influenza']
 }
 
+# Create a DataFrame for the new data
 testing_data = pd.DataFrame(data_for_outcome)
 
-testing_data['Disease_freq'] = testing_data['Disease_freq'].map(disease_freq)
-testing_data['Fever'] = encoder.fit_transform(testing_data['Fever'])
-testing_data['Cough'] = encoder.fit_transform(testing_data['Cough'])
-testing_data['Fatigue'] = encoder.fit_transform(testing_data['Fatigue'])
-testing_data['Difficulty Breathing'] = encoder.fit_transform(
+# Map 'Disease_freq' using the loaded 'disease_freq' dictionary
+testing_data['Disease_freq'] = testing_data['Disease_freq'].map(
+    category_counts)
+
+# Apply label encoding consistently
+testing_data['Fever'] = ferver_encoder.transform(testing_data['Fever'])
+testing_data['Cough'] = cough_encoder.transform(testing_data['Cough'])
+testing_data['Fatigue'] = fatigue_encoder.transform(testing_data['Fatigue'])
+testing_data['Difficulty Breathing'] = breathing_encoder.transform(
     testing_data['Difficulty Breathing'])
-testing_data['Gender'] = encoder.fit_transform(testing_data['Gender'])
-testing_data['Blood Pressure'] = encoder.fit_transform(
+testing_data['Gender'] = gender_encoder.transform(testing_data['Gender'])
+testing_data['Blood Pressure'] = blood_encoder.transform(
     testing_data['Blood Pressure'])
-testing_data['Cholesterol Level'] = encoder.fit_transform(
+testing_data['Cholesterol Level'] = fat_encoder.transform(
     testing_data['Cholesterol Level'])
 
-print(testing_data)
+
+res = clf.predict(testing_data)[0]
+res_array = np.array([res])
+
+outcome = outcome_encoder.inverse_transform(res_array)
+print(outcome[0])
 
 
