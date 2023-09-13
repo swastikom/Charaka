3664801c8981a47047a95d3a4e00dc24365f7bc4
Swastik Dhar <swastikdhar18@gmail.com>
2023-09-13 23:08:09 +0530

    unnecessary imports deleted...

--- server/main.py
+++ server/main.py
@@ -7,11 +7,7 @@ from mongoengine import connect
 # Routes Import
 
 from routes.prediction import router as prediction_router
-from routes.user import router as user_router
-from routes.user import router as user_router
-from routes.auth import router as auth_router
-from routes.password_reset import router as password_reset_router
-from routes.item import router as item_router
+
 
 
 # Running FastAPI app
@@ -32,11 +28,6 @@ app.add_middleware(
 )
 
 
-connect(db='Charaka_database', host='localhost', port=27017)
 
 app.include_router(prediction_router)
-app.include_router(user_router)
-app.include_router(auth_router)
-app.include_router(password_reset_router)
-app.include_router(item_router)
 
